46637d41a87a1bfa178a3c9a9584920899bef96e
anutikkok <anutikkok@mail.ru>
2016-12-08 20:39:13 +0300

    Добавили файлы

--- bot.py
+++ bot.py
@@ -1,5 +1,28 @@
-import telebot
 import config
+import telebot
+import db
 
-#Настраиваем соединение с ботом
+# Настраиваем соединение с нашим ботом
+bot = telebot.TeleBot(config.token)
+
+#Бот отвечает на команды
+@bot.message_handler(commands = ['start'])
+def send_welcome(message):
+    query = "SELECT info_com FROM commands WHERE id = 1"
+    result = db.query(query)
+    bot.reply_to(message, result)
+
+@bot.message_handler(commands = ['help'])
+def send_welcome(message):
+    query = "SELECT info_com FROM commands WHERE id = 2"
+    result = db.query(query)
+    bot.reply_to(message,result)
+#Бот отвечает на все сообщения по умолчанию
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+#Запускаем бота
+bot.polling()
+
+
 
